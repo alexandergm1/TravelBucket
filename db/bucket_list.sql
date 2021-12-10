5b52dd09e9dfb94fe8920bb0a9f406afac90e6c8
alexandergm1 <alexander.1gmac@gmail.com>
2021-12-10 13:11:56 +0000

    Database created and tables added

---
+++ db/bucket_list.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    continent VARCHAR(255)
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    country_id INT REFERENCES countries(id) ON DELETE CASCADE
+);
