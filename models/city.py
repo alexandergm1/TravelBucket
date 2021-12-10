41a71cb90ac1dce50e81419297ff39e297e1243f
alexandergm1 <alexander.1gmac@gmail.com>
2021-12-10 13:30:58 +0000

    Classes created

---
+++ models/city.py
@@ -0,0 +1,6 @@
+class City:
+    def __init__(self, name, country, visited, id = None):
+        self.name = name
+        self.country = country
+        self.visited = visited
+        self.id = id
---
+++ models/country.py
@@ -0,0 +1,7 @@
+class Country:
+    def __init__(self, name, continent, visited, id = None):
+        self.name = name
+        self.country = country
+        self.continent = continent
+        self.visited = visited
+        self.id = id
