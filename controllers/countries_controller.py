f6aedd480a5779d1a9722f36c82b2e3da9e9b694
alexandergm1 <alexander.1gmac@gmail.com>
2021-12-12 15:46:50 +0000

    Visited page created and functioning

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -8,5 +8,16 @@ import repositories.city_repository as city_repository
 countries_blueprint = Blueprint("countries", __name__)
 
 @countries_blueprint.route("/home")
-def countries():
+def home():
     return render_template("index.html")
+
+
+@countries_blueprint.route("/countries")
+def countries():
+    return render_template("countries/index.html")
+
+
+@countries_blueprint.route("/countries/visited")
+def countries_visited():
+    countries = country_repository.select_all()
+    return render_template("countries/visited.html", countries = countries)
