ed32411e9adf004b02e3ed9757917a2ee67bdb02
alexandergm1 <alexander.1gmac@gmail.com>
2021-12-13 11:01:50 +0000

    Added restful routes for newitems controller

--- controllers/newitems_controller.py
+++ controllers/newitems_controller.py
@@ -11,3 +11,31 @@ newitems_blueprint = Blueprint("newitems", __name__)
 @newitems_blueprint.route("/newitems")
 def newitems():
     return render_template("newitems/index.html")
+
+
+@newitems_blueprint.route("/newitems/countries")
+def newitems__cities():
+    return render_template("newitems/countries.html")
+
+@newitems_blueprint.route("/newitems/cities")
+def newitems__cities():
+    return render_template("newitems/cities.html")
+
+
+
+@newitems_blueprint.route("/newitems/countries", methods=['POST'])
+def create_country():
+    name = request.form['name']
+    continent = request.form['continent']
+    new_country = Country(name, continent)
+    country_repository.save(new_country)
+    return redirect("newitems/countries.html")
+
+
+@newitems_blueprint.route("/newitems/cities", methods=['POST'])
+def create_city():
+    name = request.form['name']
+    country  = country_repository.select(request.form['author_id'])
+    new_city = City(name, country)
+    city_repository.save(new_city)
+    return redirect("newitems/cities.html")
